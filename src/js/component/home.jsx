3b05437fb3c8944069e6d149c1b739e16df8bfdc
conpantuflas <3280kg@gmail.com>
2022-09-12 16:49:43 -0300

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import './home.css'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+  const [segundo, setSegundo] = useState(0)
+  const [minuto, setMinuto] = useState(0)
+  const [hora, setHora] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      if(segundo < 60){
+        setSegundo(segundo + 1)
+      }
+      if(segundo >= 60) {
+        setMinuto(minuto + 1)
+        setSegundo(0)
+      }
+      if(minuto >= 60) {
+        setHora(hora + 1)
+        setMinuto(0)
+      }
+      if(hora >= 24) {
+        setHora(0)
+      }
+    }, 1000)
+  })
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Counter!</h1>
+			<div className="bigContentNumber">
+			<div className="contentNumber">
+				<div className="sectionTime">
+					<p className="number">{hora}</p>
+				</div>
+				<p className="twoDots">:</p>
+				<div className="sectionTime">
+					<p className="number">{minuto}</p>
+				</div>
+				<p className="twoDots">:</p>
+				<div className="sectionTime">
+					<p className="number">{segundo}</p>
+				</div>
+			</div>
+			</div>
 		</div>
 	);
 };
